02348c58b21f8abcd86776d3942f057bf90137c0
ferolez <lopezfederico0221@gmail.com>
2024-12-12 12:16:12 -0300

    agregando leve bio

--- front/src/components/introduction.tsx
+++ front/src/components/introduction.tsx
@@ -10,10 +10,17 @@ const Introduction = () => {
         <div className='z-20 grid md:grid-cols-2 items-center mx-auto max-w-4xl h-full p-6 py-20 md:py-0 gap-4'>
          <Image src='/astronautaHome.png' alt='Home-Pic' width={500} height={500} className='rounded-md mx-auto'/>
         <div className='flex flex-col justify-center max-w-md mx-auto md:mx-0'>
-                <h1 className="text-3xl font-bold loading-tight text-center md:text-start">Hi, I&apos;m <span className="text-secondary">Fede</span></h1>
-                <p className="text-xl text-white text-center md:text-start">Together we can <TypeAnimation className="text-secondary font-semibold" 
+                <h1 className="text-3xl font-extrabold loading-tight text-center md:text-start">Hi, I&apos;m <span className="text-secondary">Fede</span></h1>
+                <p className="text-xl font-semibold text-slate-100 text-center md:text-start">Together we can <TypeAnimation className="text-secondary font-semibold" 
                 sequence={['create', 2000, 'innovate', 2000, 'build', 2000, 'work', 2000]} repeat={Infinity} cursor={false}/></p>
-        </div>
+                <p className='hidden md:flex md:text-md  text-slate-100 text-center md:text-start'>I'm a junior web developer, mainly focused on front end, user interface, and user experience</p>
+                <div className='flex justify-center md:justify-start'>
+                    <a href='/#contact' className='btn-primary'>Contact Me</a> 
+
+                </div>
+        </div> 
+
+
         </div>
    </div>
   )
